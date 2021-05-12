159726a9db9834ae3bc40164fb4bfc198ce135c1
babd0002 <babd0002@student.monash.edu>
2021-05-12 04:39:28 +0200

    updating task 3

---
+++ Assignments/Ass2A/StudentFiles/T3-cb-alter.sql
@@ -0,0 +1,31 @@
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--T3-cb-alter.sql
+
+--Student ID: 29796776
+--Student Name: Bassel Abdelkader
+--Tutorial No: 01
+
+/* Comments for your marker:
+
+
+
+
+*/
+
+/*
+Task 3:
+
+Make the listed changes to the "live" database
+*/
+--PLEASE PLACE REQUIRED SQL STATEMENT(S) BELOW
+
+-- (a)
+
+
+-- (b)
+/*
+******* Explain here your selected approach and its advantage/s *********
+*/
+
+
+-- (c)
