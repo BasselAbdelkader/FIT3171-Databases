252ad9e2c530ec9f83c528f837f19b5dc6d7e954
babd0002 <babd0002@student.monash.edu>
2021-05-28 00:04:03 +0200

    updating mh-quries.sql

--- Assignments/Ass2B/mh-queries.sql
+++ Assignments/Ass2B/mh-queries.sql
@@ -1,9 +1,9 @@
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --mh-queries.sql
 
---Student ID:
---Student Name:
---Tutorial No:
+--Student ID: 29796776
+--Student Name: Bassel Abdelkader
+--Tutorial No: 01
 
 /* Comments for your marker:
 
@@ -24,7 +24,7 @@ ht_nbr,
 e.emp_nbr,
 e.emp_lname,
 e.emp_fname,
-end_last_annual_review as review_date
+TO_CHAR(end_last_annual_review,'Dy DD Mon YYYY') AS review_date
     
 FROM
     MH.employee e 
@@ -44,7 +44,6 @@ c.client_nbr,
 c.client_lname,
 c.client_fname,
 charter_special_reqs
-
 FROM
 MH.client c
 JOIN MH.charter r ON c.client_nbr = r.client_nbr
@@ -59,7 +58,7 @@ ORDER BY
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
 SELECT
-client_fname|| ' ' || client_lname AS fullname,
+TRIM(client_fname|| ' ' || client_lname) AS fullname,
 c.charter_nbr,
 charter_cost_per_hour
 FROM
@@ -67,7 +66,9 @@ MH.charter_leg l
 JOIN MH.charter c ON l.charter_nbr = c.charter_nbr
 JOIN MH.client t ON c.client_nbr = t.client_nbr
 WHERE
-location_nbr_origin LIKE 'Mount Doom' OR location_nbr_destination LIKE 'Mount Doom' OR charter_cost_per_hour < 1000 OR  charter_special_reqs is null
+location_nbr_destination like 
+(select location_nbr from mh.location where location_name like 'Mount Doom') 
+AND (charter_cost_per_hour < 1000 OR charter_special_reqs is null)
 ORDER BY
 fullname DESC;
 
@@ -76,16 +77,15 @@ fullname DESC;
 
 */
 SELECT
-COUNT(ht.ht_nbr),
 ht.ht_nbr,
-ht.ht_name
+ht.ht_name,
+COUNT(ht.ht_nbr) as num_of_heli_owned
 FROM
 MH.helicopter h
 JOIN MH.helicopter_type ht ON ht.ht_nbr = h.ht_nbr group by  ht.ht_nbr, ht.ht_name
 HAVING COUNT(ht.ht_nbr) >=2
-
 ORDER BY
-COUNT(ht.ht_nbr) DESC;
+num_of_heli_owned DESC;
 
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
@@ -95,16 +95,16 @@ COUNT(ht.ht_nbr) DESC;
     Q5
 */
 SELECT
-COUNT(cl.location_nbr_origin),
 l.location_nbr, 
-location_name
+location_name,
+COUNT(cl.location_nbr_origin) AS num_used_as_origin
 FROM
 MH.charter_leg cl
 JOIN MH.location l ON cl.location_nbr_origin = l.location_nbr GROUP BY location_nbr,
 location_name
 HAVING COUNT (cl.location_nbr_origin) > 1
 ORDER BY
-COUNT (cl.location_nbr_origin) ASC;
+num_used_as_origin ASC;
 
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
@@ -119,22 +119,33 @@ COUNT (cl.location_nbr_origin) ASC;
 SELECT
 ht.ht_nbr,
 ht.ht_name,
-SUM(heli_hrs_flown)
+NVL(SUM(heli_hrs_flown),0) AS total_nbr_hrs_flown
 
 FROM
 MH.helicopter_type ht
-JOIN MH.helicopter h ON ht.ht_nbr = h.ht_nbr
+LEFT JOIN MH.helicopter h ON ht.ht_nbr = h.ht_nbr
 
 GROUP BY ht.ht_nbr, ht.ht_name
-ORDER BY ht.ht_nbr;
+ORDER BY total_nbr_hrs_flown ASC;
 
 /*
     Q7
 */
--- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
--- ENSURE your query has a semicolon (;) at the end of this answer
-SELECT
 
+---
+SELECT
+charter.charter_nbr,
+TO_CHAR (charter_leg.cl_atd, 'dd/mm/yyyy hh:mi:ss PM') as cl_atd
+FROM
+mh.charter JOIN mh.employee ON charter.emp_nbr = employee.emp_nbr
+JOIN mh.charter_leg on charter.charter_nbr = charter_leg.charter_nbr
+WHERE
+charter_leg.cl_atd is not null
+and charter_leg.cl_ata is not null
+and employee.emp_fname = 'Frodo'
+and employee.emp_lname= 'Baggins'
+order by
+charter_leg.cl_atd desc;
 
 /*
     Q8
@@ -148,7 +159,18 @@ SELECT
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
-
+select 
+charter.charter_nbr,
+trim(employee.emp_fname || ' ' || employee.emp_lname),
+trim(client.client_fname || ' ' || client.client_lname)
+from
+(((mh.charter_leg join mh.charter on charter_leg.charter_nbr = charter.charter_nbr) 
+join mh.client on charter.client_nbr = client.client_nbr) 
+join mh.employee on charter.emp_nbr = employee.emp_nbr)
+where
+to_char(charter_leg.cl_etd, 'dd/mon/yyyy hh:mi:sspm') = to_char(charter_leg.cl_atd, 'dd/mon/yyyy hh:mi:sspm')
+order by
+charter.charter_nbr;
 
 /*
     Q10
